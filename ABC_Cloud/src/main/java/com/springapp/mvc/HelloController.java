3446d0cf39b3aa2a2e8e871fcfe2c418bad2a437
alakkazam <bogdanpopa_cj@yahoo.com>
2015-03-12 23:15:36 +0200

    test

--- ABC_Cloud/src/main/java/com/springapp/mvc/HelloController.java
+++ ABC_Cloud/src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,10 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
-		return "hello";
+
+        System.out.println("asdfas");
+
+
+        return "hello";
 	}
 }
