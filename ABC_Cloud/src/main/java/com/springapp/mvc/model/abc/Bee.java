751fe5f872be6d0da0d1925b96ac0ec834f89ae7
dani.copaciu <dani.copaciu@gmail.com>
2015-03-28 02:19:25 +0200

    ABC Vm Migration Project - refactorization of controller module 2

--- ABC_Cloud/src/main/java/com/springapp/mvc/model/abc/Bee.java
+++ ABC_Cloud/src/main/java/com/springapp/mvc/model/abc/Bee.java
@@ -1,13 +1,14 @@
 package com.springapp.mvc.model.abc;
 
+import com.springapp.mvc.controller.Resources;
 import com.springapp.mvc.model.cloud.GreenDataCenter;
 import com.springapp.mvc.model.cloud.GreenHost;
 import com.springapp.mvc.model.cloud.GreenVm;
 import org.cloudbus.cloudsim.Datacenter;
 import org.cloudbus.cloudsim.Host;
 
-import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -21,6 +22,7 @@ public class Bee {
 
     private FoodSource foodSource;
 
+
     public Bee(int id, FoodSource foodSource) {
         this.id = id;
         this.foodSource = foodSource;
@@ -31,18 +33,26 @@ public class Bee {
 
         double prevFitnessFunction = foodSource.getFitness();
         int prevConflicts = foodSource.getConflictsNumber();
+
         int changedParameterIndex = determineChangedParameterIndex(dimension);
+
         double phi = determinePhi();
+
         Nectar changedParameter = getChangedParameter(changedParameterIndex);
+
         GreenHost prevHost = changedParameter.getHost();
+
         int prevHostId = prevHost.getId();
         int nextHostId = getNextHostId(neighbourFoodSource, changedParameterIndex);
         int newHostId = determineNewHostId(phi, prevHostId, nextHostId);
+
         GreenHost newHost = getNewHost(dataCenterList, newHostId);
+
         changedParameter.setHost(newHost);
-        double actualFitnessFunction = applyFitnessFunction(dataCenterList);
+
         int actualConflict = computeConflicts();
-        if (prevFitnessFunction > actualFitnessFunction && actualConflict <= prevConflicts) {
+        double actualFitnessFunction = applyFitnessFunction(dataCenterList);
+        if (prevFitnessFunction > actualFitnessFunction) {
             changedParameter.setHost(prevHost);
             applyFitnessFunction(dataCenterList);
             computeConflicts();
@@ -51,7 +61,6 @@ public class Bee {
             foodSource.setTrialsNumber(0);
 
         }
-
     }
 
     private Nectar getChangedParameter(int changedParameterIndex) {
@@ -71,38 +80,14 @@ public class Bee {
         return nextHost.getId();
     }
 
-    /**
-     * deep copy the previous host
-     *
-     * @param host is the object that is cloned
-     * @return the copy of the host
-     * @throws IOException
-     * @throws ClassNotFoundException
-     */
-    private GreenHost savePreviousHost(GreenHost host) throws IOException, ClassNotFoundException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        oos.writeObject(host);
-        oos.flush();
-        oos.close();
-        bos.close();
-        byte[] byteData = bos.toByteArray();
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(byteData);
-        Object object = new ObjectInputStream(inputStream).readObject();
-        if (object instanceof GreenHost) {
-            return (GreenHost) new ObjectInputStream(inputStream).readObject();
-        }
-        return null;
-    }
-
     private int determineNewHostId(double phi, int prevHostId, int nextHostId) {
         int newHostId;
         newHostId = (int) (prevHostId + phi * (prevHostId - nextHostId));
         if (newHostId < 0) {
             newHostId = 0;
         }
-        if (newHostId >= Constants.HOST_NUMBER) {
-            newHostId = Constants.HOST_NUMBER - 1;
+        if (newHostId >= Resources.HOST_NUMBER) {
+            newHostId = Resources.HOST_NUMBER - 1;
         }
         return newHostId;
     }
@@ -114,7 +99,7 @@ public class Bee {
 
     private double determinePhi() {
         Random random = new Random();
-        return random.nextDouble();
+        return (random.nextDouble() - 0.5) * 2;
     }
 
     public FoodSource getFoodSource() {
@@ -138,17 +123,30 @@ public class Bee {
         List<Nectar> nectarList = foodSource.getNectarList();
         double result = 0;
         for (GreenDataCenter dc : dataCenterList) {
+            double maxLatency = 0;
             double vmEnergy = 0;
             for (Nectar n : nectarList) {
                 GreenHost host = n.getHost();
                 String currentDataCenterName = host.getDatacenter().getName();
-                if (currentDataCenterName.equals(dc.getName())) {
+                if (currentDataCenterName != null && currentDataCenterName.equals(dc.getName())) {
                     GreenVm vm = n.getVm();
                     double necessaryEnergy = vm.getNecessaryEnergy();
                     vmEnergy += necessaryEnergy;
+                    if (n.getLatency() > maxLatency) {
+                        maxLatency = n.getLatency();
+                    }
                 }
             }
-            double energy = vmEnergy / dc.getGreenEnergyQuantity();
+            double energy;
+            if (dc.getGreenEnergyQuantity() <= 0) {
+                energy = 0;
+            } else {
+                energy = vmEnergy / dc.getGreenEnergyQuantity();
+            }
+            energy -= maxLatency;
+            if (energy < 0) {
+                energy = 0;
+            }
             result += energy;
         }
         foodSource.setFitness(result);
@@ -167,5 +165,28 @@ public class Bee {
         }
         foodSource.setConflictsNumber(conflicts);
         return conflicts;
+    }
+
+    public double computeProbability(List<FoodSource> foodSourceList) {
+        double fitnessSum = 0;
+        for (FoodSource fs : foodSourceList) {
+            double fitnessValue = fs.getFitness();
+            fitnessSum += fitnessValue;
+        }
+        FoodSource thisFoodSource = getFoodSource();
+        double thisFitnessValue = thisFoodSource.getFitness();
+        double thisProbability = thisFitnessValue / fitnessSum;
+        thisFoodSource.setProbability(thisProbability);
+        return thisProbability;
+    }
+
+    public double computeLatency() {
+        List<Double> latencyArray = new ArrayList<Double>();
+        for (Nectar n : foodSource.getNectarList()) {
+            latencyArray.add(n.getLatency());
+        }
+        double latency = Collections.max(latencyArray);
+        foodSource.setLatencyParam(latency / 300);
+        return foodSource.getLatencyParam();
     }
 }
